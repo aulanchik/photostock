66500dd00f47dbf142d644c8bf1306fe1f2ac0c7
Artyom Ulanchik <artyom.u@outlook.com>
2024-06-14 00:29:40 +0100

    added Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">PhotoStocks</h1>
+    </header>
+  );
+};
+
+export default Header;
