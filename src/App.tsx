1fc2547ed831de6368d238f5410d5ad1fadeae03
Artyom Ulanchik <artyom.u@outlook.com>
2024-06-14 00:31:43 +0100

    added grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-import React, { JSX } from "react";
+import React, { useCallback, useRef } from "react";
+import { Upload, Header, Footer } from "@/components";
+import { Grid } from "@/containers";
 
-const App: React.FC = (): JSX.Element => <></>;
+const App: React.FC = (): JSX.Element => {
+  const gridRef = useRef<{ fetchImages: () => void }>(null);
+
+  const handleUploadComplete = useCallback(() => {
+    gridRef.current?.fetchImages();
+  }, []);
+
+  return (
+    <main>
+      <Header />
+      <Upload onUploadComplete={handleUploadComplete} />
+      <Grid ref={gridRef} />
+      <Footer />
+    </main>
+  );
+};
 
 export default App;
