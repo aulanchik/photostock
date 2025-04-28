bc6b6ba57b100544eb42fd9f4412e6e33f3502bb
Artyom Ulanchik <artyom.u@outlook.com>
2025-04-28 14:20:58 +0300

    combine all

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
-import React, { JSX } from "react";
+import { FC } from "react";
+import Grid from "@/components/grid";
+import Header from "@/components/header";
+import UploadDialog from "@/components/upload";
+import Footer from "@/components/footer";
+import { useFetch } from "@/hooks/useFetch";
 
-const App: React.FC = (): JSX.Element => <></>;
+const App: FC = (): JSX.Element => {
+    const { images, loading, error, fetchImages } = useFetch();
+
+    const handleUploadComplete = () => {
+        fetchImages();
+    };
+
+    return (
+        <>
+            <Header />
+            <UploadDialog onUploadComplete={handleUploadComplete} />
+            <Grid images={images} loading={loading} error={error} />
+            <Footer />
+        </>
+    );
+};
 
 export default App;
